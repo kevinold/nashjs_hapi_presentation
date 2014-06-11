f870be49ad829d75fd35511bf457aac79a9b09cd
Kevin Old <kevin.old@coupa.com>
2014-06-11 16:12:25 -0500

    more slides and fixes

--- demos/3-docs.js
+++ demos/3-docs.js
@@ -1,9 +1,10 @@
 var Hapi = require('hapi');
+var Joi = require('joi');
 
 // Create a server with a host and port
 var server = Hapi.createServer('localhost', 9000);
 
-server.route({
+server.route([{
   method: 'GET',
   path: '/hello',
   config: {
@@ -12,7 +13,36 @@ server.route({
     },
     cache: { expiresIn: 60 * 5 * 1000 } // 
   }
-});
+},
+{
+  method: 'GET',
+  path: '/hello2',
+  config: {
+    handler: function (request, reply) {
+      reply('hello world');
+    },
+    cache: { expiresIn: 60 * 5 * 1000 } // 
+  }
+},
+{
+  method: 'GET',
+  path: '/hello-world',
+  config: {
+    validate: {
+        params: {
+            name: Joi.string().min(8).max(100)
+        },
+        query: {
+            mood: Joi.string().valid(["neutral","happy","sad"])
+                              .default("neutral")
+        }
+    },
+    handler: function (request, reply) {
+      reply('hello world');
+    }
+  }
+}
+]);
 
 // Start the server with lout
 server.pack.require('lout', function() {
