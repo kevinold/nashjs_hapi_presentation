111b359468b8eca8a76ad9bea8914545bac4a7d1
Kevin Old <kevin@kevinold.com>
2014-06-11 15:26:22 -0500

    more slides

---
+++ demos/3-docs.js
@@ -0,0 +1,20 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = Hapi.createServer('localhost', 9000);
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  config: {
+    handler: function (request, reply) {
+      reply('hello world');
+    },
+    cache: { expiresIn: 60 * 5 * 1000 } // 
+  }
+});
+
+// Start the server with lout
+server.pack.require('lout', function() {
+    server.start();
+});
